92a0d34622993cf506cbc6289ead64b530463894
Emoto13 <emcho_spassov@abv.bg>
2020-08-24 11:33:07 +0300

    Modify users model and serializer

--- users/serializers.py
+++ users/serializers.py
@@ -8,7 +8,8 @@ class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password',
-                  'is_staff', 'is_active', 'last_login', 'date_joined',
-                  'birthday', 'account_title', 'latitude', 'longitude',
+        fields = ['id', 'username', 'first_name', 'last_name', 'email',
+                  'is_staff', 'is_superuser', 'is_active',
+                  'last_login', 'date_joined', 'birthday', 'profile_image',
+                  'account_title', 'latitude', 'longitude',
                   'gender', 'country']
