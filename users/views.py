743940492dcd4bc0aa2357420f4e12e9a78e5c11
Emoto13 <emcho_spassov@abv.bg>
2020-08-24 11:33:29 +0300

    Add GET and POST api views

--- users/views.py
+++ users/views.py
@@ -1,34 +1,50 @@
+from functools import partial
+
 from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import CustomUser
 from users.serializers import CustomUserSerializer
 
-from django.contrib.auth import authenticate
+from django.contrib.auth import logout
 
 
 class CustomUsersViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
-
-
-@permission_classes([IsAuthenticated])
-@api_view(['POST'])
-def auth_user(request):
-    print(request.data, request.headers, request.user)
-    #  user = authenticate(username=request.data['username'], password=request.data['password'])
-    # request.user = user
-    # print(user)
-    print(request.user)
-    return Response('Authenticated successfully', status=status.HTTP_200_OK)
+    permission_classes = []
 
 
 @api_view(['POST'])
 def create_user(request):
-    user = CustomUser.objects.create_user(**request.data)
+    CustomUser.objects.create_user(**request.data)
     return Response('User creation successful', status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def get_user(request, username):
+    user = CustomUser.objects.get(username=username)
+    if user is None:
+        return Response("No user with that username", status=status.HTTP_200_OK)
+    return Response(CustomUserSerializer(user).data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def create_user(request):
+    CustomUser.objects.create_user(**request.data)
+    return Response('User creation successful', status=status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+def delete_user(request):
+    pass
+
+
+@api_view(['GET'])
+def user_logout(request):
+    logout(request)
+    return Response('Logged out successfully', status=status.HTTP_200_OK)
